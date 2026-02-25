0c8551a6b09324e1330a8a23c7faff16d6912cc2
Aayush <aayushbastola772@gmail.com>
2026-02-25 20:54:53 +0545

    added models

---
+++ backend/src/models/transaction.ts
@@ -0,0 +1,15 @@
+export type transaction_type="credit"|"debit";
+
+export interface transaction{
+    id:string;
+    user_id:string;
+    category_id:string;
+    title:string;
+    remarks:string;
+    amount: number;
+    type:transaction_type;
+    txn_date:string;
+    created_at:string;
+    updated_at:string;
+}
+
