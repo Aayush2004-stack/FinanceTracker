850b29b9ed9398740de87b363ed9672d7729d5c1
Aayush <aayushbastola772@gmail.com>
2026-02-25 23:55:26 +0545

    generate jwt after registration

--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -1,6 +1,24 @@
 import { user } from "../models/user";
 import { pool } from "../configs/db";
 import {hashPassword} from "../utils/hashPw"
+import jwt, {SignOptions} from "jsonwebtoken"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export function signToken(
+  userId:string,
+): string{
+  const options: SignOptions ={
+    expiresIn: process.env.JWT_EXPIRES_IN as SignOptions["expiresIn"]
+  }
+
+  return jwt.sign(
+    {userId},
+    process.env.JWT_SECRET!,
+     options);
+
+}
 
 export async function registerUser(input: {
   name: string;
@@ -15,11 +33,13 @@ export async function registerUser(input: {
   const q = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) returning 
     *;`;
 
-  const result = await pool.query<user>(q, [name.trim(), email, hashPassword]);
+  const result = await pool.query<user>(q, [name.trim(), email, hashedPassword]);
 
   const user = result.rows[0];
+  const token = signToken(user.id)
 
   return {
+    user_token:token,
     id: user.id,
     name: user.name,
     email: user.email,
