3a2d5d25af6e7bdc3c564f6d11ceaa012bb4f352
Aayush <aayushbastola772@gmail.com>
2026-03-01 17:50:48 +0545

    unique violation err handling

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express"
 import {registerUser}  from "../services/authService" 
+import {HttpError} from "../utils/errors"
 
 export async function  register(req:Request, res:Response){
     const {name, email, password}= req.body;
@@ -13,8 +14,20 @@ export async function  register(req:Request, res:Response){
     if(!password){
         return res.status(400).json({message:"Password is required"})
     }
-    const userData= await registerUser({name, email, password})
-    return res.status(201).json({message:"User registered successfully",userData})
+    try{
+
+        const userData= await registerUser({name, email, password})
+        return res.status(201).json({message:"User registered successfully",userData})
+    
+    }
+    catch(err){
+        if (err instanceof HttpError){
+            return res.status(err.status).json({message:err.message})
+        }
+        console.log(err);
+        return res.status(500).json({message:"Server error:"})
+
+    }
 
 
 
--- backend/src/services/authService.ts
+++ backend/src/services/authService.ts
@@ -3,6 +3,7 @@ import { pool } from "../configs/db";
 import {hashPassword} from "../utils/hashPw"
 import jwt, {SignOptions} from "jsonwebtoken"
 import dotenv from "dotenv";
+import {HttpError,  isPgUniqueVoilation} from "../utils/errors";
 
 dotenv.config();
 
@@ -25,25 +26,37 @@ export async function registerUser(input: {
   email: string;
   password: string;
 }) {
-  const { name, email, password } = input;
-  //TODO: validate input fields
 
-  const hashedPassword= hashPassword(password);
 
-  const q = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) returning 
+    const { name, email, password } = input;
+    //TODO: validate input fields
+    
+    const hashedPassword= hashPassword(password);
+    try{
+    
+    const q = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) returning 
     *;`;
+    
+    const result = await pool.query<user>(q, [name.trim(), email, hashedPassword]);
+    
+    const user = result.rows[0];
+    const token = signToken(user.id)
+    
+    return {
+      user_token:token,
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+  }
+  catch(err){
+    if(isPgUniqueVoilation(err)){
+      throw new HttpError(409,"Email is already used")
 
-  const result = await pool.query<user>(q, [name.trim(), email, hashedPassword]);
+    }
+    throw err;
 
-  const user = result.rows[0];
-  const token = signToken(user.id)
-
-  return {
-    user_token:token,
-    id: user.id,
-    name: user.name,
-    email: user.email,
-    created_at: user.created_at,
-    updated_at: user.updated_at,
-  };
+  }
 }
