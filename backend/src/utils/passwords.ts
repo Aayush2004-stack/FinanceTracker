c4ca9729edb55fdbd0f319483f53943504fb1740
Aayush <aayushbastola772@gmail.com>
2026-03-03 16:32:45 +0545

    verify password for login

--- backend/src/utils/passwords.ts
+++ backend/src/utils/passwords.ts
@@ -4,6 +4,11 @@ export async function hashPassword(password: string){
     return bcrypt.hash(password,10);
 }
 
+
+export async function verifyPassword(password:string, hashedPassword: string){
+    return bcrypt.compare(password,hashedPassword);
+
+}
 // export  function validatePassword(password:string):boolean{
 //     if(password.length<8){
 //         return false;
