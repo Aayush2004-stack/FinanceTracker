9afa344a859efac2cd49193dd39942719cc7409f
Aayush <aayushbastola772@gmail.com>
2026-03-04 11:55:09 +0545

    changed get to post

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -7,6 +7,6 @@ const router = Router();
 
 router.post("/register", register);
 router.put("/register/validate", authMiddleware,validateUserEmail);
-router.get("/login/", login);
+router.post("/login/", login);
 
 export default router;
