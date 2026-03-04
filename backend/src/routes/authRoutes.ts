0727775ecb4400e8de56d822d8ceb750d9a6e79d
Aayush <aayushbastola772@gmail.com>
2026-03-04 15:31:47 +0545

    send otp routes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
+import {sendOtp} from "../controllers/otpController"
 import {register,validateUserEmail, login} from "../controllers/authController";
 import {authMiddleware} from "../middlewares/authMiddleware"
+
 
 const router = Router();
 
 router.post("/register", register);
-router.put("/register/validate", authMiddleware,validateUserEmail);
+router.post("/send-otp", sendOtp )
+router.put("/validate",validateUserEmail);
 router.post("/login/", login);
 
 export default router;
