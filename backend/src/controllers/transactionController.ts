27b7e9cd66a09662435d4aed3a715572fb70fe84
Aayush <aayushbastola772@gmail.com>
2026-06-05 21:09:39 +0545

    ADD: validation for amt,type, txnDate

--- backend/src/controllers/transactionController.ts
+++ backend/src/controllers/transactionController.ts
@@ -9,6 +9,18 @@ export async function addTransaction(req: Request, res: Response, next: NextFunc
         if(!amount || !type || !categoryId || !areaId || !txnDate){
             throw new HttpError(400, "All fields are required!");
         }
+        if(type !== "credit" && type !== "debit"){
+            throw new HttpError(400, "Invalid transaction type!");
+        }
+        if(isNaN(amount)){
+            throw new HttpError(400, "Amount should be a number!");
+        }
+        if(isNaN(Date.parse(txnDate))){
+            throw new HttpError(400, "Invalid date format!");
+        }
+        if(amount <= 0){
+            throw new HttpError(400, "Amount should be greater than zero!");
+        }
         const result = await transactionService.addTransaction(userId,  categoryId, areaId,title, remark, amount, type, txnDate);
         return res.status(201).json(result);
     }
