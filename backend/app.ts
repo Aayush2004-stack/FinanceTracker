f47b05d0db7facbb7a2bdb7d7b983d39bd5f291b
Aayush <aayushbastola772@gmail.com>
2026-06-27 20:24:22 +0545

    Add: rate limiter

--- backend/app.ts
+++ backend/app.ts
@@ -1,26 +1,31 @@
 import { testDbConnection } from "./src/configs/db";
 import dotenv from "dotenv";
 import express from "express";
-import register from "./src/routes/authRoutes"
-import {errorHandler} from "./src/middlewares/errorMiddleware"
-import cors from "cors"
+import register from "./src/routes/authRoutes";
+import { errorHandler } from "./src/middlewares/errorMiddleware";
+import cors from "cors";
 import categoryRoutes from "./src/routes/categoryRoutes";
 import areaRoutes from "./src/routes/areaRoutes";
 import transactionRoutes from "./src/routes/transactionRoutes";
+import { apiLimiter, authLimiter } from "./src/middlewares/rateLimiter";
 
 dotenv.config();
 
 const app = express();
 
 // Enable CORS for all routes
-app.use(cors({
-  origin: "http://localhost:5173", // replace with your frontend URL
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true, // if you need cookies
-}));
-app.use(express.json());//parse the json body
+app.use(
+  cors({
+    origin: "http://localhost:5173", // replace with your frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // if you need cookies
+  }),
+);
+app.use(express.json()); //parse the json body
 
-app.use("/api/auth", register);
+app.use(apiLimiter);
+
+app.use("/api/auth", authLimiter, register);
 
 app.use("/api/categories", categoryRoutes);
 app.use("/api/areas", areaRoutes);
